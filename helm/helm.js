a92d5f2bb1e1dca6fcdd29ffb2313fba274dacf8
Jonathan Catherine <yannickcatherine@gmail.com>
2020-05-08 17:21:22 -0400

    Test

--- helm/helm.js
+++ helm/helm.js
@@ -9,16 +9,16 @@ const github = require('@actions/github');
 
         const owner = core.getInput('owner');
         const repo = core.getInput('repo');
-        const branch = core.getInput('branch');
+        const ref = core.getInput('branch');
 
 
 
         const payload = JSON.stringify(github.context.payload, undefined, 2);
         console.log(`The event owner: ${owner}`);
         console.log(`The event repo: ${repo}`);
-        console.log(`The event branch: ${branch}`);
+        console.log(`The event branch: ${ref}`);
 
-        const ref = await octokit.git.getRef({ owner, repo, branch });
+        const ref = await octokit.git.getRef({ owner, repo, ref });
 
         //console.log(`Ref: ${ref}`);
     } catch (error) {
