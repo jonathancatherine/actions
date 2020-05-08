b5a0c2f2f65def023c8ae9c10ce2b3a27f90434c
Jonathan Catherine <yannickcatherine@gmail.com>
2020-05-08 16:53:17 -0400

    Test

--- helm/helm.js
+++ helm/helm.js
@@ -1,26 +1,14 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
+const github = require('@actions/github');
 
 (async () => {
     try {
-        const directory = core.getInput('directory');
-        const registryHost = core.getInput('registryHost');
-        const registryPath = core.getInput('registryPath');
-        const dockerImageName = core.getInput('dockerImageName');
-        const registryUsername = core.getInput('registryUsername');
-        const registryPassword = process.env.REGISTRY_PASSWORD || core.getInput('registryPassword');
+        const githubToken = process.env.GITHUB_TOKEN;
+        const octokit = new github.GitHub(githubToken);
 
-        const registry = `${registryHost}/${registryPath}/${dockerImageName}`;
-
-        const tag = Math.round(Math.random() * 10);
-        const tagBuild = `${registry}:${tag}`;
-        const tagLatest = `${registry}:latest`;
-
-        await exec.exec(`docker login ${registryHost} -u ${registryUsername} -p ${registryPassword}`);
-        await exec.exec(`docker build --cache-from ${tagLatest} -t ${tagBuild} ${directory}`);
-        await exec.exec(`docker tag ${tagBuild} ${tagLatest}`);
-        await exec.exec(`docker push ${tagBuild}`);
-        await exec.exec(`docker push ${tagLatest}`);
+        const payload = JSON.stringify(github.context.payload, undefined, 2)
+        console.log(`The event payload: ${payload}`);
     } catch (error) {
         core.setFailed(error.message);
     }
