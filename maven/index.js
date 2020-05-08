133e1bad55b703b5852750c8d6b20c12daff2753
Jonathan Catherine <yannickcatherine@gmail.com>
2020-05-07 22:42:53 -0400

    Test

--- maven/index.js
+++ maven/index.js
@@ -5,6 +5,7 @@ const exec = require('@actions/exec');
 (async () => {
     try {
         const directory = core.getInput('directory');
+        await exec.exec(`pwd`);
         await exec.exec(`ls ${directory}`);
         await exec.exec(`mvn -B package --file ${directory}/pom.xml -DskipTests`);
     } catch (error) {
