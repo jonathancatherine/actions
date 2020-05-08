78bcefac811e209071ba8c466192a62bd2f05248
Jonathan Catherine <yannickcatherine@gmail.com>
2020-05-07 22:21:20 -0400

    Test

---
+++ maven/index.js
@@ -0,0 +1,10 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+try {
+    const directory = core.getInput('directory');
+    await exec.exec(`mvn -B package --file ${directory}/pom.xml -DskipTests`);
+} catch (error) {
+    core.setFailed(error.message);
+}
