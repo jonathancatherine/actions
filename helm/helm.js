a62b36fbce00d24efd3cadf45560a86a8d6594fc
Jonathan Catherine <yannickcatherine@gmail.com>
2020-05-08 23:20:38 -0400

    Test

--- helm/helm.js
+++ helm/helm.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 const github = require('@actions/github');
+const yaml = require('yaml');
 
 (async () => {
     try {
