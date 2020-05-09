6fbe7d9a36fcd6bc0381d2ba59185967edc66b07
Jonathan Catherine <yannickcatherine@gmail.com>
2020-05-09 15:11:10 -0400

    Test

--- src/mavenDockerHelm.ts
+++ src/mavenDockerHelm.ts
@@ -1,8 +1,9 @@
 import * as core from "@actions/core";
 import * as github from "@actions/github";
 import * as maven from "../src/utils/maven";
+import * as docker from "../src/utils/docker";
 
-function mavenBuild() {
+async function mavenBuild() {
     const mavenPomFile = core.getInput('mavenPomFile');
     const skipTests = core.getInput('skipTests');
 
@@ -15,13 +16,35 @@ function mavenBuild() {
     maven.build(mavenParameters);
 }
 
+async function dockerBuild(tag: string) {
+    const dockerFile = core.getInput('dockerFile');
+    const dockerImage = core.getInput('dockerImage');
+    const registryHost = core.getInput('registryHost');
+    const registryUsername = core.getInput('registryUsername');
+    const registryPassword = core.getInput('registryPassword');
+
+    const dockerOptions: docker.DockerOptions = {
+        dockerFile: dockerFile,
+        dockerImage: dockerImage,
+        registryHost: registryHost,
+        registryPassword: registryPassword,
+        registryUsername: registryUsername,
+        tag: tag
+    };
+    await docker.buildAndPush(dockerOptions);
+}
 
 async function run(): Promise<void> {
     try {
-        mavenBuild();
+        await mavenBuild();
 
-        const githubToken = process.env.GITHUB_TOKEN || "";
-        const octokit = new github.GitHub(githubToken);
+
+        await dockerBuild("sdfsdfs");
+
+
+
+        //const githubToken = process.env.GITHUB_TOKEN || "";
+        //const octokit = new github.GitHub(githubToken);
 
         // const payload = JSON.stringify(github.context.payload, undefined, 2)
         // console.log(`The event payload: ${payload}`);
